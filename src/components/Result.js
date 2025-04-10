760cd47a1c0aa42597dc8fbac006c2826471ba7d
ArthurDombroski <tutudombroski@gmail.com>
2025-04-09 21:07:03 -0300

    criação de uma nova função

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
+function classificarIMC(imc) {
+  if (imc < 18.5) {
+    return "Abaixo do peso";
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    return "Peso normal";
+  } else if (imc >= 25 && imc <= 29.9) {
+    return "Sobrepeso";
+  } else if (imc >= 30 && imc <= 34.9) {
+    return "Obesidade grau 1";
+  } else if (imc >= 35 && imc <= 39.9) {
+    return "Obesidade grau 2";
+  } else {
+    return "Obesidade grau 3 (obesidade mórbida)";
+  }
+}
+
 const Result = ({ imc }) => {
   return (
     <Text style={styles.result}>
       Seu IMC é: {imc}
+    </Text>
+  );
+};
+
+const Tabela = ({ imc }) => {
+  return (
+    <Text style={styles.result}>
+      {classificarIMC}
     </Text>
   );
 };
